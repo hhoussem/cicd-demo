4913f39fbcd3f40c7200377c18dd7d4bcf7c0187
hhoussem <houssemeddine.hamrouni@gmail.com>
2020-02-29 16:08:05 +0100

    fix script path

--- ops/docker_build_start.sh
+++ ops/docker_build_start.sh
@@ -1,6 +1,6 @@
 #force remove of current container if exists
 docker rm -f demo
 #docker will build image (name "demo-image")
-docker build -t demo-image ../.
+docker build -t demo-image .
 #docker will run the "demo-image" on deamon mode (background) under port 9999 and will name the created container "demo"
 docker run -d -p 9999:9999 --name demo demo-image
